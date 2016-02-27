95838eb708da87bcc5d9a41d10a5336c65d17542
Kawamura Shintaro <s.kawamura@zanon.co.jp>
2016-02-28 00:49:47 +0900

    create components

---
+++ src/js/components/todo-input-form.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ContainerJS from 'container-js';
+
+export default React.createClass({
+    propTypes: {
+        actions: React.PropTypes.object.isRequired
+    },
+
+    getInitialState() {
+        return {
+            error: '',
+            title: ''
+        }
+    },
+
+    render() {
+        return (
+            <div className="todo-input-form">
+                title:
+                &nbsp;
+                <input type="text" className="title-field" value={this.state.title} onChange={this.onChange}></input>
+                &nbsp;&nbsp;
+                <button onClick={this.newTodo} className="add-button">
+                    add
+                </button>
+                <div className="error">{this.state.error}</div>
+            </div>
+        );
+    },
+
+    onChange(event) {
+        this.setState({
+            error: '',
+            title: event.target.value
+        });
+    },
+
+    newTodo(event) {
+        try {
+            this.props.actions.add(this.state.title);
+            this.setState({error: '', title: ''});
+        } catch (e) {
+            this.setState({error: e.message, title: ''});
+        }
+    }
+});
